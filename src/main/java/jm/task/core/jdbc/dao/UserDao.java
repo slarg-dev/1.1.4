41a92c7c723c04751e1f9f73c887a2d6d53fb2f2
slarg <ghade04@mail.ru>
2023-03-10 18:20:14 +0300

    changes interface

--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -11,14 +11,9 @@ import java.util.List;
 public interface UserDao {
     SessionFactory sessionFactory = Util.getConnection();
     void createUsersTable() throws SQLException;
-
     void dropUsersTable() throws SQLException;
-
     void saveUser(String name, String lastName, byte age) throws SQLException;
-
     void removeUserById(long id) throws SQLException;
-
     List<User> getAllUsers() throws SQLException;
-
     void cleanUsersTable() throws SQLException;
 }
--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -8,14 +8,9 @@ import java.util.List;
 public interface UserService {
     UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     void createUsersTable();
-
     void dropUsersTable();
-
     void saveUser(String name, String lastName, byte age);
-
     void removeUserById(long id);
-
     List<User> getAllUsers();
-
     void cleanUsersTable();
 }
